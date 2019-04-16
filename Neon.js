1d9da2c935048fc855a7444a7c07fdb9db438b7c
DUB1401 <vlad.milosta@yandex.ru>
2019-04-17 00:24:32 +0300

    FLEX

--- Neon.js
+++ Neon.js
@@ -128,6 +128,7 @@ function LoadChapter() {
     
 }
 
+//Выравнивание высоты обложек новелл в библиотеке.
 $(function(){
   $('.BookLeftLayout').height($('.BookLeftLayout').width()/0.75);
 
@@ -139,34 +140,55 @@ $(function(){
 //Выравнивание названий новелл.
 function VarticalAlignBookHeads() {
     var Boxes = document.getElementsByClassName('LibruaryBookName');
-    
+
     for (var i = 0; i < Boxes.length; i++) {
         Boxes[i].style.height = "auto";
     }
 
     var Numero;
 
-    if (Boxes.length % 4 == 0) {
-        Numero = Boxes.length / 4;
-    } else {
-        Numero = Math.floor(Boxes.length / 4 + 1);
-    }
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        //Если двухколонный режим (мини-экраны).
+        if (Boxes.length % 2 == 0) {
+            Numero = Boxes.length / 2;
+        } else {
+            Numero = Math.floor(Boxes.length / 2 + 1);
+        }
 
-    for (var u = 0; u < Numero; u++) {
-        var Height = 0;
-        for (var i = 4 * u; i < 4 * u + 4; i++) {
-            if (i < Boxes.length && Boxes[i].offsetHeight > Height) {
-                Height = Boxes[i].offsetHeight;
+        for (var u = 0; u < Numero; u++) {
+            var Height = 0;
+            for (var i = 2 * u; i < 2 * u + 2; i++) {
+                if (i < Boxes.length && Boxes[i].offsetHeight > Height) {
+                    Height = Boxes[i].offsetHeight;
+                }
+            }
+
+            for (var i = 2 * u; i < 2 * u + 2; i++) {
+                if (i < Boxes.length) Boxes[i].style.height = Height + "px";
+            }
+
+        }
+    } else {
+        //Если четырёхколонный режим (PC).
+        if (Boxes.length % 4 == 0) {
+            Numero = Boxes.length / 4;
+        } else {
+            Numero = Math.floor(Boxes.length / 4 + 1);
+        }
+
+        for (var u = 0; u < Numero; u++) {
+            var Height = 0;
+            for (var i = 4 * u; i < 4 * u + 4; i++) {
+                if (i < Boxes.length && Boxes[i].offsetHeight > Height) {
+                    Height = Boxes[i].offsetHeight;
+                }
+            }
+
+            for (var i = 4 * u; i < 4 * u + 4; i++) {
+                if (i < Boxes.length) Boxes[i].style.height = Height + "px";
             }
         }
-
-        for (var i = 4 * u; i < 4 * u + 4; i++) {
-            if (i < Boxes.length) Boxes[i].style.height = Height + "px";
-        }
-
     }
-
-    
 }
 
 //Копирование строки в буфер обмена.
