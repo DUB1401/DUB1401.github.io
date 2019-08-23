380a5a25f25b51e2aa1f5000079dac46b4887bf4
DUB1401 <vlad.milosta@yandex.ru>
2019-08-23 23:59:54 +0300

    neon-upd-008

--- Neon.js
+++ Neon.js
@@ -137,60 +137,6 @@ $(function(){
   });
 });
 
-//Выравнивание названий новелл.
-function VarticalAlignBookHeads() {
-    var Boxes = document.getElementsByClassName('LibruaryBookName');
-
-    for (var i = 0; i < Boxes.length; i++) {
-        Boxes[i].style.height = "auto";
-    }
-
-    var Numero;
-
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        //Если двухколонный режим (мини-экраны).
-        if (Boxes.length % 2 == 0) {
-            Numero = Boxes.length / 2;
-        } else {
-            Numero = Math.floor(Boxes.length / 2 + 1);
-        }
-
-        for (var u = 0; u < Numero; u++) {
-            var Height = 0;
-            for (var i = 2 * u; i < 2 * u + 2; i++) {
-                if (i < Boxes.length && Boxes[i].offsetHeight > Height) {
-                    Height = Boxes[i].offsetHeight;
-                }
-            }
-
-            for (var i = 2 * u; i < 2 * u + 2; i++) {
-                if (i < Boxes.length) Boxes[i].style.height = Height + "px";
-            }
-
-        }
-    } else {
-        //Если четырёхколонный режим (PC).
-        if (Boxes.length % 4 == 0) {
-            Numero = Boxes.length / 4;
-        } else {
-            Numero = Math.floor(Boxes.length / 4 + 1);
-        }
-
-        for (var u = 0; u < Numero; u++) {
-            var Height = 0;
-            for (var i = 4 * u; i < 4 * u + 4; i++) {
-                if (i < Boxes.length && Boxes[i].offsetHeight > Height) {
-                    Height = Boxes[i].offsetHeight;
-                }
-            }
-
-            for (var i = 4 * u; i < 4 * u + 4; i++) {
-                if (i < Boxes.length) Boxes[i].style.height = Height + "px";
-            }
-        }
-    }
-}
-
 //Копирование строки в буфер обмена.
 function TextCopy(URLtext) {
     var copytext = document.createElement('input')
