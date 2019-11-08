9027dc9a863c27046aff35690e4a68545162018b
DUB1401 <vlad.milosta@yandex.ru>
2019-11-09 02:06:55 +0300

    neon-upd-020

--- Releases.js
+++ Releases.js
@@ -1,7 +1,7 @@
 var Releases = "";
 
+Releases = Releases + "Арифурэта: Сильнейший ремесленник в мире$Омакэ. Купание$09.11.2019$arifureta-shokugyou-de-sekai-saikyou=25$30.jpg@";
 Releases = Releases + "Дневник потерянных глаз$Глава 3. Аномальная активность$07.17.2019$diary-of-lost-eyes=4$31.jpg@";
-Releases = Releases + "Арифурэта: Сильнейший ремесленник в мире$Глава 21. Страж глубин$06.11.2019$arifureta-shokugyou-de-sekai-saikyou=22$30.jpg@";
 Releases = Releases + "Сказание о странствующем мастере ки$Глава 20. Северный волк$08.10.2019$the-legend-of-the-wandering-ki-master=20$29.jpg@";
 Releases = Releases + "Вещает станция «Восход»$7. Правильно$19.08.2019$broadcasting-station-sunrise=32$11.jpg@";
 
