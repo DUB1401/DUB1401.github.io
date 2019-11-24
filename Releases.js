7156310973f09b4433c24ef176c55f9d26a254e6
DUB1401 <vlad.milosta@yandex.ru>
2019-11-24 19:23:06 +0300

    neon-dev-005

--- Releases.js
+++ Releases.js
@@ -1,5 +1,6 @@
 var Releases = "";
 
+Releases = Releases + "Аватар короля: Ради славы$Глава 2. Единожды став друзьями, остались противниками на всю жизнь. Часть 1$24.11.2019$the-kings-avatar-for-the-glory=2$32.jpg@";
 Releases = Releases + "Арифурэта: Сильнейший ремесленник в мире$Омакэ. Купание$09.11.2019$arifureta-shokugyou-de-sekai-saikyou=25$30.jpg@";
 Releases = Releases + "Дневник потерянных глаз$Глава 3. Аномальная активность$07.17.2019$diary-of-lost-eyes=4$31.jpg@";
 Releases = Releases + "Сказание о странствующем мастере ки$Глава 20. Северный волк$08.10.2019$the-legend-of-the-wandering-ki-master=20$29.jpg@";
@@ -11,7 +12,6 @@ Releases = Releases + "Тысячелетие$Глава 10. Первый раб
 
 Releases = Releases + "Вещает станция «Восход»$6. Умираю молодым$06.06.2019$broadcasting-station-sunrise=31$11.jpg@";
 Releases = Releases + "Вещает станция «Восход»$5. Никто не вспомнит о тебе$05.06.2019$broadcasting-station-sunrise=30$11.jpg@";
-Releases = Releases + "Вещает станция «Восход»$4. Дохлятина$22.04.2019$broadcasting-station-sunrise=29$11.jpg@";
 
 //Releases = Releases + "Я побрился. Затем привёл домой старшеклассницу$Глава 7. Косметика$18.04.2019$i-shaved-then-i-brought-a-high-school-girl-home=8$20.jpg@";
 
