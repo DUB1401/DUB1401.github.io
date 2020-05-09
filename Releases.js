3a8c896000edf224819996e2cc1912010dd8e5cb
DUB1401 <vlad.milosta@yandex.ru>
2020-05-10 00:05:31 +0300

    neon-upd-053

--- Releases.js
+++ Releases.js
@@ -1,5 +1,8 @@
 var Releases = "";
 
+
+Releases = Releases + "Последний император Сэнгоку$Глава 52. Выгодное предложение$10.05.2020$the-last-emperor-sengoku=55$29.jpg@";
+
 Releases = Releases + "Последний император Сэнгоку$Пролог: Ниндзя в столице$03.05.2020$the-last-emperor-sengoku=54$29.jpg@";
 Releases = Releases + "Последний император Сэнгоку$Эпилог: Воссточная экспедиция$02.05.2020$the-last-emperor-sengoku=53$29.jpg@";
 
@@ -13,8 +16,6 @@ Releases = Releases + "Сказание о странствующем масте
 
 Releases = Releases + "Сказание о странствующем мастере ки$Глава 45. Как снег на голову$25.03.2020$the-last-emperor-sengoku=46$29.jpg@";
 Releases = Releases + "Сказание о странствующем мастере ки$Глава 44. Глина и немного мошенничества$25.03.2020$the-last-emperor-sengoku=45$29.jpg@";
-
-Releases = Releases + "Сказание о странствующем мастере ки$Глава 43. Винные откровения$20.03.2020$the-last-emperor-sengoku=44$29.jpg@";
 
 //Releases = Releases + "Зима юности Мацуи-куна$Глава 1. Промежуточные тесты$18.12.2019$the-winter-of-matsui-kuns-youth=2$33.jpg@";
 //Releases = Releases + "Арифурэта: Сильнейший ремесленник в мире$Бонусная история. Страшный кошмар в бездне$08.12.2019$arifureta-shokugyou-de-sekai-saikyou=33$30.jpg@";
