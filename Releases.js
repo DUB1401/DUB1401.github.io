b09be9f5a82c58d8ea528d082ac02e0b94e2c40b
DUB1401 <vlad.milosta@yandex.ru>
2020-08-06 00:54:59 +0300

    neon-upd-057

--- Releases.js
+++ Releases.js
@@ -1,5 +1,6 @@
 var Releases = "";
 
+Releases = Releases + "Последний император Сэнгоку$Глава 65. Подготовка к осаде$05.08.2020$the-last-emperor-sengoku=69$29.jpg@";
 Releases = Releases + "Последний император Сэнгоку$Глава 64. Важное решение$04.08.2020$the-last-emperor-sengoku=68$29.jpg@";
 Releases = Releases + "Последний император Сэнгоку$Глава 63. Колода пересмешника$03.08.2020$the-last-emperor-sengoku=67$29.jpg@";
 Releases = Releases + "Последний император Сэнгоку$Глава 62. Туз в рукаве$02.08.2020$the-last-emperor-sengoku=66$29.jpg@";
@@ -11,8 +12,6 @@ Releases = Releases + "Последний император Сэнгоку$Гл
 
 Releases = Releases + "Вещает станция «Восход»$10. Безмолвный крик$30.05.2020$broadcasting-station-sunrise=35$11.jpg@";
 Releases = Releases + "Последний император Сэнгоку$Глава 58. Предчувствие беды$29.06.2020$the-last-emperor-sengoku=62$29.jpg@";
-
-Releases = Releases + "Последний император Сэнгоку$Глава 57. Работа, от которой не отказываются$22.06.2020$the-last-emperor-sengoku=61$29.jpg@";
 
 //Releases = Releases + "Зима юности Мацуи-куна$Глава 1. Промежуточные тесты$18.12.2019$the-winter-of-matsui-kuns-youth=2$33.jpg@";
 //Releases = Releases + "Арифурэта: Сильнейший ремесленник в мире$Бонусная история. Страшный кошмар в бездне$08.12.2019$arifureta-shokugyou-de-sekai-saikyou=33$30.jpg@";
